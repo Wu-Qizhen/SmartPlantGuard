b8744cddad3752827ef8ebefc5d73c1d01583758
Wu Qizhen <3162946784@qq.com>
2026-02-13 12:16:59 +0800

    Initial commit: CLion 集成

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,7 +31,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "cmsis_os2.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
@@ -41,7 +41,8 @@ extern "C" {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
-
+extern osMessageQueueId_t Queue_SensorDataHandle;
+extern osMessageQueueId_t Queue_BluetoothRxHandle;
 /* USER CODE END EC */
 
 /* Exported macro ------------------------------------------------------------*/
---
+++ Tasks/task_comm.c
@@ -0,0 +1,12 @@
+/**
+* 代码不注释，同事两行泪！（给！爷！写！）
+ * Elegance is not a dispensable luxury but a quality that decides between success and failure!
+ * Created by Wu Qizhen on 2026.02.13
+ */
+#include "cmsis_os2.h"
+
+void StartTask_Comm(void *argument) {
+    for (;;) {
+        osDelay(10);
+    }
+}
